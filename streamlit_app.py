0fad47cb7a9356bf92b30f8c2a60e4bb1dd586f5
Abdo_Halem10 <116370015+AbdoHalem@users.noreply.github.com>
2024-10-18 20:55:39 +0000

    "Adding dataset and data analysis"

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas as pd
 
 st.title('🤖 Machine Learning App')
 st.info('This is app predicts the annual salary of a person whether is is more than 50K dollars or less !')

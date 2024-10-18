710f3f1a4bb9105d09cb09b06f30016dbad5122d
Abdo_Halem10 <116370015+AbdoHalem@users.noreply.github.com>
2024-10-18 23:37:40 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,4 +4,6 @@ st.title('🤖 Machine Learning App')
 st.info('This is app predicts the annual salary of a person whether is is more than 50K dollars or less !')
 
 with st.expander("Data"):
-    
+    st.write('**Raw Data**')
+    data = pd.read_csv("cesus.csv")
+    data
